c5c29cd5a97b38d3aa629080111404aab9d43af2
Rafael Jesus <rafaelljesus86@gmail.com>
2016-08-09 09:21:53 -0300

    Fix listener initialize params

--- lib/clark/event_bus/listener.rb
+++ lib/clark/event_bus/listener.rb
@@ -3,11 +3,11 @@ require 'bunny'
 module Clark
   module EventBus
     class Listener
-      def initialize(options: {})
+      def initialize(options = {})
         @session = Bunny.new(options)
       end
 
-      def on(queue_name, options, &block)
+      def on(queue_name, options = {}, &block)
         raise ArgumentError.new('queue name must be present') unless queue_name && queue_name.size > 0
         queue(queue_name).subscribe(options, &block)
       end
